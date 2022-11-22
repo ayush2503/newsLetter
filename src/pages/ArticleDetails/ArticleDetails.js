1115ca307ba4f2f3697b2bd17bc4d498a8205098
devStar2022 <devstar102022@gmail.com>
2022-11-22 15:54:21 +0530

    changed some styling in article detail page

--- src/pages/ArticleDetails/ArticleDetails.js
+++ src/pages/ArticleDetails/ArticleDetails.js
@@ -28,14 +28,14 @@ function ArticleDetails() {
 if(!data)
     return <div>no data</div>
   return (
-    <Box sx={{bgcolor:"white",p:2,boxShadow:10}}>
+    <Box sx={{bgcolor:"white",p:2,boxShadow:10,m:5}}>
         <Typography sx={{textTransform:"uppercase",fontWeight:500,fontSize:14}}>{data?.tag}</Typography>
         <Typography sx={{fontSize:24,fontWeight:500,mt:1,mb:0.1}}>{data?.heading}</Typography>
         <Typography sx={{fontSize:12}}>{moment(data?.createdAt?.seconds*1000).format("LL")}</Typography>
         <CardMedia
          component="img"
          height="325"
-         image="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png"
+         image={data?.image ?data.image:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png"}
          alt=""
          sx={{mt:2,mb:1}}
         />
